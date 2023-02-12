1ba0f09a8aff117ed5f4e245085bed0a51fb087d
buithutruc <112782993+buithutruc@users.noreply.github.com>
2023-02-11 20:06:45 -0500

    practiced TS types

--- src/main.ts
+++ src/main.ts
@@ -12,5 +12,18 @@
 let myName: string;
 let meaningOfLife: number;
 let isLoading: boolean;
+let album: any;
 
 myName = "John";
+meaningOfLife = 46;
+isLoading = true;
+album = 1984;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean | string;
+
+let re: RegExp = /\w+/g;
